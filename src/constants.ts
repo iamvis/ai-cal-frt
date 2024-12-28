087de673507e74e11d01250e0a149ad58a1cce6e
iamvis <prjapativishal82141@gmail.com>
2024-12-28 11:58:53 +0530

    dev

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+const SWATCHES =[
+    '#FF6347', // Tomato
+    '#FFD700', // Gold
+    '#800080', // Purple
+    '#008000', // Green
+    '#FFA500', // Orange
+    '#0000FF', // Blue
+    '#FF00FF', // Magenta
+    '#808000', // Olive
+    '#800000', // Maroon
+    '#00FFFF', // Cyan
+    '#ADD8E6', // Light Blue
+];
+export {SWATCHES};
